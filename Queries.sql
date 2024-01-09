17b1629b8d3d3975f9c8ee9f7634c231b7a47f20
1rn21cs018alan <1rn21cs018alan@gmail.com>
2024-01-09 08:07:27 +0530

    Added Username and password table also created constraint names

--- Queries.sql
+++ Queries.sql
@@ -6,24 +6,24 @@ create table Book(
     Title varchar(70) not null,
     Edition int,
     Available bool default true,
-    primary key(Book_id)
+    constraint pk1 primary key(Book_id)
 );
 create table Author(
 	Author_id int,
     Author_name varchar(200),
-    primary key(Author_id)
+    constraint pk2 primary key(Author_id)
 );
 create table Publisher(
 	Publisher_id int,
     Publisher_name varchar(200),
-    primary key(Publisher_id)
+    constraint pk3 primary key(Publisher_id)
 );
 create table Library_member(
 	Member_id int auto_increment,
     Fname varchar(30) not null,
     Mname varchar(30),
     Lname varchar(30),
-    primary key(Member_id)
+    constraint pk4 primary key(Member_id)
 );
 create table Borrowed_by(
 	Transaction_id int,
@@ -31,51 +31,59 @@ create table Borrowed_by(
     Borrowing_date date,
     Book_id int,
     Member_id int,
-	primary key(Transaction_id),
-    foreign key(Book_id) references Book(Book_id),
-    foreign key(Member_id) references Library_member(Member_id)
+	constraint pk5 primary key(Transaction_id),
+    constraint fk1 foreign key(Book_id) references Book(Book_id),
+    constraint fk2 foreign key(Member_id) references Library_member(Member_id)
 );
 create table Written_by(
 	Author_id int,
     Book_id int,
-    foreign key(Book_id) references Book(Book_id),
-    foreign key(Author_id) references Author(Author_id),
-	primary key(Book_id,Author_id)
+	constraint pk6 primary key(Book_id,Author_id),
+    constraint fk3 foreign key(Book_id) references Book(Book_id),
+    constraint fk4 foreign key(Author_id) references Author(Author_id)
 );
 alter table Book add column Publisher_id int;
-alter table Book add foreign key(Publisher_id) references Publisher(Publisher_id);
+alter table Book add constraint fk5 foreign key(Publisher_id) references Publisher(Publisher_id);
 
 create table Genre(
 	Genre_id int,
     Genre_name varchar(30),
-    primary key(Genre_id)
+    constraint pk7 primary key(Genre_id)
 );
 
 create table Book_genre(
 	Book_id int,
     Genre_id int,
-    foreign key(Book_id) references Book(Book_id),
-    foreign key(Genre_id) references Genre(Genre_id),
-    primary key(Book_id,Genre_id)
+    constraint pk8 primary key(Book_id,Genre_id),
+    constraint fk6 foreign key(Book_id) references Book(Book_id),
+    constraint fk7 foreign key(Genre_id) references Genre(Genre_id)
 );
 create table Member_address(
 	Member_id int,
     address varchar(200) not null,
-    foreign key(Member_id) references Library_member(Member_id),
-    primary key(Member_id,address)
+    constraint pk9 primary key(Member_id,address),
+    constraint fk8 foreign key(Member_id) references Library_member(Member_id)
 );
 create table Member_email(
 	Member_id int,
     email varchar(70) not null,
-    foreign key(Member_id) references Library_member(Member_id),
-    primary key(Member_id,email)
+    constraint pk10 primary key(Member_id,email),
+    constraint fk9 foreign key(Member_id) references Library_member(Member_id)
 );
 create table Member_phone_number(
 	Member_id int,
     phone varchar(15) not null,
-    foreign key(Member_id) references Library_member(Member_id),
-    primary key(Member_id,phone)
+    constraint pk11 primary key(Member_id,phone),
+    constraint fk10 foreign key(Member_id) references Library_member(Member_id)
 );
+create table user_cred(
+	id int,
+    user_name varchar(30) unique not null,
+    password varchar(30) unique not null,
+    constraint pk12 primary key(id),
+    constraint fk11 foreign key(id) references Library_member(Member_id)
+);
+
 insert into library_member values(1001,'James','','Ackerson');
 insert into library.member_address values(1001,'Bightin House, 4th Avenue');
 insert into library.member_email values(1001,'james@gmail.com');
@@ -90,3 +98,4 @@ insert into library.member_email values(1001,'destroyer@gmail.com');
 insert into library.member_phone_number values(1001,'+443839397698');
 
 insert into library.library_member(fname,mname,lname) values('James','Stone','Hyoren');
+insert into library.user_cred values (1001,"jamesj","Lib_pass1");
